b612c29f0b830c9b1e4c533def2a5f923ecef7e1
John Doe <johndoe@example.com>
2023-10-04 15:16:50 +0300

    fix

--- src/js/page/createCatalogPage.js
+++ src/js/page/createCatalogPage.js
@@ -1,5 +1,7 @@
+import {createCardProduct} from '../components/createCardProduct.js';
 import {createDescr} from '../components/createDescr.js';
 import {createMainTitle} from '../components/createMainTitle.js';
+import {productArray} from '../productArray.js';
 
 export const createCatalogPage = () => {
   const catalog = document.createElement('div');
@@ -8,9 +10,16 @@ export const createCatalogPage = () => {
   const descr = createDescr('Страница находится в разработке');
 
   catalog.classList.add('catalog-page', 'page');
-  catalogList.classList.add('catalog-page__list', 'list-reset');
+  catalogList.classList.add(
+      'catalog-page__list',
+      'main-page__list',
+      'list-reset');
 
   catalog.append(title, descr, catalogList);
+
+  productArray.forEach(card => {
+    catalogList.append(createCardProduct(card));
+  });
 
   return {
     catalog,
